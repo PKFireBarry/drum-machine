40fad0eef8d342a166bbf7fa05bf51473e11e644
Darion George <Barry0719@gmail.com>
2023-05-25 10:18:48 -0400

    Added sounds

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import H1  from '../src/sounds/Heater-1.mp3';
+import H2 from '../src/sounds/Heater-2.mp3';
+import H3 from '../src/sounds/Heater-3.mp3';
+import H4 from '../src/sounds/Heater-4_1.mp3';
+import CH from '../src/sounds/Cev_H2.mp3';
+import OH from '../src/sounds/Dsc_Oh.mp3';
+import Kick1 from '../src/sounds/Kick_n_Hat.mp3';
+import Kick2 from '../src/sounds/RP4_KICK_1.mp3';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="drum-machine"> 
+        <div id='display'></div>
+        <div id='drum-pad-container'>
+
+        </div>
+      </div>
     </div>
   );
 }
